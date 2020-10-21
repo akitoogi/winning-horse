1d8b933dc92c822eca49389c44b28b9b3c152788
akitoogi <agito2358@yahoo.co.jp>
2020-10-21 17:55:22 +0900

    today's code

--- crawler/get_race_results/get_race_results/spiders/netkeiba.py
+++ crawler/get_race_results/get_race_results/spiders/netkeiba.py
@@ -4,7 +4,10 @@ import scrapy
 class NetkeibaSpider(scrapy.Spider):
     name = 'netkeiba'
     allowed_domains = ['https://race.netkeiba.com']
-    start_urls = ['https://race.netkeiba.com/top/']
+    start_urls = ['https://race.netkeiba.com/race/result.html?race_id=202008040411']
 
     def parse(self, response):
-        pass
+        for race_result in response.xpath('//*[@id="All_Result_Table"]/tbody/tr'):
+            print('--------------------------------------')
+            result = race_result.xpath('td/div').getall()
+            print(result)
